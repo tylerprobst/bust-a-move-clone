420a8a8b305bc2f91db3c9b066101daea06dca60
Tyler <TylerProbst@Tylers-MacBook-Pro.local>
2016-04-20 18:42:16 -0700

    adds todo list

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
+	
+	/*visible canvas area*/
 	GameCanvas = function () {
 			this.canvas = document.createElement('canvas');
 			this.context = this.canvas.getContext('2d');
-		};
+			this.grid = new GameGrid();
+	};
+
+	/* populates canvas with visible orbs */
+	GameCanvas.prototype.fillCanvas = function () {
+
+	};
 
 	GameCanvas.prototype.start = function () {
 		this.canvas.setAttribute('id', 'canvas');
@@ -10,44 +18,57 @@ $(document).ready(function () {
 		this.canvas.height = 500;
 		
 		$('#canvas-area').html(this.canvas);
-	}
+	};
 
+	/* grid to store orbs and locations */
 	GameGrid = function (rows, cols) {
 		this.rows = rows;
 		this.cols = cols;
 		this.grid = [];
 	};
 
-	GameGrid.prototype.renderOrbs = function () {
+	/*populates grid with orbs*/
+	GameGrid.prototype.fillGrid = function () {
 
 	};
 
-	Orb = function(color) {
-		this.position  = [];
+    /* Orb Class */
+	Orb = function(color, position) {
+		this.position  = position; //array [x,y]
 		this.color     = color;
 		this.neighbors = [];
 		this.diameter  = 30;
 	};
 
+	/* Draws singular Orb */
 	Orb.prototype.drawOrb = function (orbX, orbY) {
 		context = gameCanvas.context;
-		context.arc(16, 16, orb.diameter/2, 0, 2 * Math.PI, false);
-	    context.fillStyle = 'green';
+		context.arc(orbX, orbY, this.diameter/2, 0, 2 * Math.PI, false);
+	    context.fillStyle = this.color;
 	    context.fill();
-	    context.lineWidth = 1;
+	    context.lineWidth = 2;
 	    context.strokeStyle = '#003300';
 	    context.stroke();
 	};
-
-
-
-
+//						TO-DO:
+//-------------------------------------------------------
+//calculate individual orbs position
+//function for randomizing orb color
+//function for filling the board with randommized orbs
+//fill grid according to how many orbs fit in a row
+//calculate how many orbs fit in a row
+//at start of game fill 5 rows with orbs
+//offset every other row (orb.diameter/2)
 
 	//start game! 
 	$('#start-button').click(function () {
 		gameCanvas = new GameCanvas;
 		gameCanvas.start();
-		orb = new Orb;
-		orb.drawOrb();
-	})
+		orb = new Orb('red', [20,20]);
+		orb.drawOrb(orb.position[0], orb.position[1]);
+	});
+
+
+
+
 })
